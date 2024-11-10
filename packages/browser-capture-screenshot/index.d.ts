5930e38a85fc30233d0dfa4bb86bce15a3ad286b
amoshydra <8733840+amoshydra@users.noreply.github.com>
2024-11-10 19:01:53 +0800

    fix: remove cjs exports
    BREAKING CHANGE: cjs will no longer be provided, CJS environment can load module via dynamic import

--- packages/browser-capture-screenshot/index.d.ts
+++ packages/browser-capture-screenshot/index.d.ts
@@ -1,4 +1,1 @@
-export function captureScreenshot(element: HTMLElement): Promise<string>;
-export function setup(): Promise<[MediaStream, MediaStreamTrack]>;
-export function focus(element: HTMLElement | false, stream: MediaStream): Promise<void>;
-export function rasterize(element: HTMLElement, stream: MediaStream): Promise<string>;
+export * from "./index.mjs";
