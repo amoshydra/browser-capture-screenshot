3dfb397063d0b7b3cdd7e4ffb80db2d2fcc1194a
amoshydra <8733840+amoshydra@users.noreply.github.com>
2024-11-12 09:07:06 +0800

    fix: declare export type manually

--- packages/browser-capture-screenshot/index.d.ts
+++ packages/browser-capture-screenshot/index.d.ts
@@ -1,1 +1,8 @@
-export * from "./index.mjs";
+export function capture(element: HTMLElement): Promise<string>
+
+export class ScreenshotSession {
+  static isSupported(): boolean;
+  start(): Promise<void>
+  stop()
+  capture(element: HTMLElement): Promise<string>
+}
